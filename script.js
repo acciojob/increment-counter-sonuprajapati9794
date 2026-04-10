3d0b2a70163c4d3048219a90b80747f63f67dc23
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-10 12:56:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+// Get elements
+const counter = document.getElementById("counter");
+const button = document.getElementById("incrementBtn");
+
+// Add click event
+button.addEventListener("click", function () {
+  
+  // Get current value (before increment)
+  let currentValue = parseInt(counter.innerText);
+
+  // Show alert with un-incremented value
+  alert("Current Value: " + currentValue);
+
+  // Increment value
+  currentValue++;
+
+  // Update counter
+  counter.innerText = currentValue;
+});
